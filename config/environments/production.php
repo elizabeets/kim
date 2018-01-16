0eb62d467c9e311a3b5ad3290b14be1664329ec4
Eyal Gantz <eyal@madebywil.co>
2018-01-16 17:58:21 -0500

    src: force SSL in production

--- config/environments/production.php
+++ config/environments/production.php
@@ -7,3 +7,11 @@ define('SCRIPT_DEBUG', false);
 define('DISALLOW_FILE_MODS', true);
 define( 'WP_MEMORY_LIMIT', '256M' );
 define( 'WP_POST_REVISIONS', 3 );
+
+define( 'FORCE_SSL_ADMIN', true );
+define( 'FORCE_SSL_LOGIN', true );
+
+if ( $_SERVER['HTTP_X_FORWARDED_PROTO'] && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) {
+	$_SERVER['HTTPS']       = 'on';
+	$_SERVER['SERVER_PORT'] = 443;
+}
