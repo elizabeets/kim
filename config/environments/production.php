910af3cd4af876c95d4b30d3e3d1d4db4fdfe890
Eyal Gantz <eyal@madebywil.co>
2018-05-11 21:09:48 -0400

    fix: force https in production

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,17 +1,19 @@
 <?php
 /** Production */
-ini_set('display_errors', 0);
-define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', false);
+ini_set( 'display_errors', 0 );
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'SCRIPT_DEBUG', false );
 /** Disable all file modifications including updates and update notifications */
-define('DISALLOW_FILE_MODS', true);
+define( 'DISALLOW_FILE_MODS', true );
 define( 'WP_MEMORY_LIMIT', '256M' );
 define( 'WP_POST_REVISIONS', 3 );
 
-define( 'FORCE_SSL_ADMIN', true );
-define( 'FORCE_SSL_LOGIN', true );
+$cloudfront_proto = $_SERVER['HTTP_CLOUDFRONT_FORWARDED_PROTO'] ?? 'http';
+$server_proto     = $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? 'http';
 
-if ( $_SERVER['HTTP_X_FORWARDED_PROTO'] && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) {
+if ( $cloudfront_proto === 'https' || $server_proto === 'https' ) {
 	$_SERVER['HTTPS']       = 'on';
 	$_SERVER['SERVER_PORT'] = 443;
+	define( 'FORCE_SSL_ADMIN', true );
+	define( 'FORCE_SSL_LOGIN', true );
 }
